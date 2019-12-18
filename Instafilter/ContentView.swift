f1b9fd3cebf53bea142151751424e3159d2c0c44
Sam McCullough <samanuel.mc@gmail.com>
2019-12-17 22:07:06 -0800

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Sam on 2019-12-17.
+//  Copyright © 2019 Sailfish Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
